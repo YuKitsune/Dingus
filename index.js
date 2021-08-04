33e301c93a1fd6f1df8496c6fa6119a5f0944d71
Eoin Motherway <eoin@yukitsune.dev>
2021-08-04 22:13:39 +1000

    Initial commit
    ping pong!

---
+++ index.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('messageCreate', async message => {
+	if (!client.application?.owner) await client.application?.fetch();
+
+	if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner.id) {
+		const data = {
+			name: 'ping',
+			description: 'Replies with Pong!',
+		};
+
+		const command = await client.guilds.cache.get(message.guild.id)?.commands.create(data);
+		console.log(command);
+	}
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(process.env.TOKEN);
